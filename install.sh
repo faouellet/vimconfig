92aba132322cd607482cd94e62e95e4c2dccd914
faouellet <faouellet@gmail.com>
2015-11-05 17:42:15 -0800

    Updating the install script

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+# Create a .vim folder if one is not present
+cd ~
+mkdir .vim
+
+# Transfer the content to the .vim folder
+mv vimconfig/* .vim/
+cd .vim/
+rmdir vimconfig/
+
 # Create symlink for the dotfiles
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.ycm_extra_conf.py ~/.ycm_extra_conf.py
@@ -7,20 +16,6 @@ git submodule init
 git submodule update --init --recursive
 
 # Install YouCompleteMe
-cd ~
+cd bundle/YouCompleteMe/
+./install.py --clang-completer
 
-if ! test -d ${LLVM_SRC}; then
-    mkdir ycm_temp
-    cd ycm_temp
-    wget llvm.org/releases/3.5.0/clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz 
-    tar xvf clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    mv clang+llvm-3.5.0-x86_64-linux-gnu/* .
-    rm clang+llvm-3.5.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    rmdir clang+llvm-3.5.0-x86_64-linux-gnu/
-    cd ~
-fi
-
-mkdir ycm_build
-cd ycm_build
-cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-make ycm_support_libs
