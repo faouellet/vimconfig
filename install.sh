a4c86d6f32f0f836b45e037f3155f788e5325cc9
faouellet <faouellet@gmail.com>
2016-12-23 20:04:47 -0500

    Fixing some errors in the install script

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 # Remove old .vim directory if one already exists and then create an empty one
-cd ~
-if [ -d ".vim" ]; then
-  rm -rf ".vim"
-fi
-mkdir ".vim"
+#cd ~
+#if [ -d ".vim" ]; then
+#  rm -rf ".vim"
+#fi
+#mkdir ".vim"
 
 # Install Neovim
-apt-get install software-properties-common
-add-apt-repository ppa:neovim-ppa/unstable
-apt-get install neovim
+#apt-get install software-properties-common
+#add-apt-repository ppa:neovim-ppa/unstable
+#apt-get install neovim
 
 # Transfer the contents of my vim config to the .vim folder
 mv vimconfig/* .vim/
 cd .vim/
-rmdir vimconfig/
+rm -rf vimconfig/
 
 # Create symlink for the .vimrc
 ln -s ~/.vim/.vimrc ~/.vimrc
 
-# Get Vundle  plugin manager
+# Get Vundle plugin manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Have Vundle fetch the plugins
-nvim -i NONE -c VundleUpdate -c quitall
+nvim +PluginInstall +qall
 
 # Install YouCompleteMe
 cd bundle/YouCompleteMe/
